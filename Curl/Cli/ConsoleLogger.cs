449073a5072c926cde2db251b515a2f19143c4c0
Trup10ka <lukys3036@gmail.com>
2024-12-18 23:26:41 +0100

    Update console logger colors - now only apply for prefix

--- Curl/Cli/ConsoleLogger.cs
+++ Curl/Cli/ConsoleLogger.cs
@@ -1,3 +1,5 @@
+using Curl.Data.Commands;
+
 namespace Curl.Cli;
 
 public static class ConsoleLogger
@@ -5,21 +7,32 @@ public static class ConsoleLogger
     public static void LogInfo(string message)
     {
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"INFO: {message}");
+        Console.Write("INFO: ");
         Console.ResetColor();
+        Console.WriteLine(message);
+    }
+    
+    public static void LogInfo(CommandResult result)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("INFO: ");
+        Console.ResetColor();
+        Console.WriteLine(result.Result);
     }
     
     public static void LogWarning(string message)
     {
         Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine($"WARN: {message}");
+        Console.Write("WARN: ");
         Console.ResetColor();
+        Console.WriteLine(message);
     }
 
     public static void LogError(string message)
     {
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"ERROR: {message}");
+        Console.Write("ERROR: ");
         Console.ResetColor();
+        Console.WriteLine(message);
     }
 }

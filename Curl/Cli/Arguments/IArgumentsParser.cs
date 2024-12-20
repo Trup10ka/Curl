774032473f8cc02b3bcfeeb4c4f1f7b1c087f512
Trup10ka <lukys3036@gmail.com>
2024-12-20 21:54:26 +0100

    Implement command arguments parser

---
+++ Curl/Cli/Arguments/IArgumentsParser.cs
@@ -0,0 +1,8 @@
+﻿using CommandType = Curl.Cli.Commands.CommandType;
+
+namespace Curl.Cli.Arguments;
+
+public interface IArgumentsParser
+{
+    Dictionary<string, string?> ParseArguments(CommandType command, string[] args);
+}
---
+++ Curl/Cli/Arguments/StrictPairArgumentParser.cs
@@ -0,0 +1,49 @@
+﻿using Curl.Cli.Commands;
+using Curl.Exceptions.Arguments;
+
+namespace Curl.Cli.Arguments;
+
+public class StrictPairArgumentParser : IArgumentsParser
+{
+    private bool CanHaveNoArguments { get; }
+    
+    public StrictPairArgumentParser(bool canHaveNoArguments)
+    {
+        CanHaveNoArguments = canHaveNoArguments;
+    }
+    
+    public Dictionary<string, string?> ParseArguments(CommandType command, string[] args)
+    {
+        if (args.Length == 0 && !CanHaveNoArguments)
+        {
+            throw new EmptyArgumentsException(command);
+        }
+
+        var strictPairArgs = new Dictionary<string, string?>();
+
+        for (var i = 0; i < args.Length; i++)
+        {
+            var token = args[i];
+
+            if (token.StartsWith('-') && token.Length == 2)
+            {
+                var flag = token[1].ToString();
+
+                if (strictPairArgs.ContainsKey(flag))
+                    throw new DuplicateArgumentException(flag);
+                
+                if (i + 1 < args.Length && !args[i + 1].StartsWith('-'))
+                {
+                    strictPairArgs[flag] = args[i + 1];
+                    i++;
+                }
+                else
+                    throw new MissingArgumentValueException(flag);
+            }
+            else
+                throw new UnknownArgumentException(token);
+        }
+
+        return strictPairArgs;
+    }
+}
